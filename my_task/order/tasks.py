48fec523ebc9376fdbefb0e27de20a49dacacf3e
LH667371 <773426798@qq.com>
2021-04-28 11:48:56 +0800

    修改定时代码，测试

--- my_task/order/tasks.py
+++ my_task/order/tasks.py
@@ -1,7 +1,23 @@
+import datetime
+
 from my_task.main import app
+from order.models import Order
+
+
+# celery与Django结合使用  识别并加载django的配置文件
+
 
 @app.task(name="check_order")
 def check_order():
     """未支付订单超时取消"""
     print('正在运行 未支付订单超时取消 任务')
-    pass
+    order = Order.objects.filter(create_time__lt=datetime.datetime.now() - datetime.timedelta(hours=1))
+    print(order)
+    for i in order:
+        if datetime.datetime.now() - i.create_time > datetime.timedelta(minutes=1) and i.order_status == 0:
+            change = Order.objects.get(pk=i.id)
+            change.order_status = 3
+            change.save()
+
+if __name__ == '__main__':
+    check_order()
